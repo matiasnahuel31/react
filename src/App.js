30dbd322d2c93cbb97ea79ea944dbb03a240dd2b
Matias Melgarejo <matizeta15@gmail.com>
2022-06-16 22:53:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Elijo creer - Qatar 2022
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=a6blYZmCNjI&ab_channel=matiaxification"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          give me freedom, give me fire
         </a>
       </header>
     </div>
