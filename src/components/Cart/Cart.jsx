19a1ee5854a71faad43252c6bce561c61f771b90
Matias Melgarejo <matizeta15@gmail.com>
2022-07-27 01:29:25 -0300

    firebase 2

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,27 @@
+import {collection, getFirestore} from 'firebase/firestore';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../cartContext/CartContext';
 import ItemCart from '../ItemCart';
 const Cart = () => {
     const {cart, totalPrice} = useCartContext();
+
+    const order = {
+        buyer: {
+            name: 'luis',
+            email: 'luis02@gmail.com',
+            phone: '1148693020',
+        },
+        items: cart.map(product => ({id: product.id, description: product.description, price: product.price,quantity: product.quantity})),
+        total: totalPrice(),
+    }
+
+    const handleClick = () => {
+        const db = getFirestore();
+        const orderCollection = collection(db,'orders');
+        addDoc(orderCollection,order)
+        .then(({id})=>console.log(id))
+    }
 
     if(cart.length === 0){
     return(
@@ -20,6 +38,7 @@ const Cart = () => {
             }
             <p>
                 total: {totalPrice()}
+                <button onClick={handleClick}>Emitir compra</button>
             </p>
         </>
     )
